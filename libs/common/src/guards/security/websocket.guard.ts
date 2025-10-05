2824fc2e365204dc819dc4dae59e563bf968f79e
JuliusDgenius <ibejulius1@gmail.com>
2025-10-05 10:30:22 +0100

    Extend websocket guard to handle SSE + WS

--- libs/common/src/guards/security/websocket.guard.ts
+++ libs/common/src/guards/security/websocket.guard.ts
@@ -5,22 +5,38 @@ import { verify } from 'jsonwebtoken';
 @Injectable()
 export class WebSocketGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const client = context.switchToWs().getClient();
-    const token = client.handshake?.auth?.token;
+    const type = context.getType<'ws' | 'http'>();
 
-    if (!token) {
-      throw new WsException('Authentication token not found');
-    }
+    // Handle Websocket
+    if (type == 'ws') {
+      const client = context.switchToWs().getClient();
+      const token = client.handshake?.auth?.token;
 
-    try {
-      if (!process.env.JWT_SECRET) {
-        throw new WsException('JWT_SECRET not configured');
+      if (!token) {
+        throw new WsException('Authentication token not found');
       }
-      const decoded = verify(token, process.env.JWT_SECRET);
+      const decoded = verify(token, process.env.JWT_SECRET!);
       client.user = decoded;
       return true;
-    } catch (err) {
-      throw new WsException('Invalid token');
     }
+    
+    // Handle HTTP/SSE auth
+    if (type === 'http') {
+      const request = context.switchToHttp().getRequest();
+      const authHeader = request.headers['authorization'];
+
+      if (!authHeader) return false;
+      const token = authHeader.split(' ')[1];
+
+      try {
+        const decoded = verify(token, process.env.JWT_SECRET!);
+        request.user = decoded;
+        return true;
+      } catch {
+        return false;
+      }
+    }
+
+    return false;
   }
 } 
